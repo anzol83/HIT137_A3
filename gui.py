2942124470d523807f540e7622432995b658b34a
LEGION <rahuladhk0@gmail.com>
2026-01-22 19:18:47 +0930

    creating menu and function button

--- gui.py
+++ gui.py
@@ -1,9 +1,25 @@
 import tkinter as tk
 
-def tk_gui():
-    root=tk.Tk()
-    root.title("Image Editor")
-    root.geometry("600x400")
-    root.mainloop()
+class gui_system():
+    def __init__(self, root):
+        self.root=root
+        self.root.title("Image Editor")
+        self.root.geometry("600x400")
+        
+        self.window()
+        self.layout()
+    
+    def window(self):
+        menu=tk.Menu(self.root)
+        
+        menu.add_cascade(label="File")
+        menu.add_cascade(label="Edit")
 
-tk_gui()
+        self.root.config(menu=menu)
+
+    def layout(self):
+        tk.Button(text="Greyscale").pack(side=tk.LEFT)
+        tk.Button(text="Blur").pack(side=tk.LEFT)
+        tk.Button(text="Edges").pack(side=tk.LEFT)
+        tk.Button(text="Rotate").pack(side=tk.LEFT)
+        tk.Button(text="Flip").pack(side=tk.LEFT)
---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from gui import gui_system
+
+if __name__=="__main__":
+    root=tk.Tk()
+    app=gui_system(root)
+    root.mainloop()
