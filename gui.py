fa7c82dfab2534224bb6714641bb5093d57507d6
LEGION <rahuladhk0@gmail.com>
2026-01-24 15:22:05 +0930

    adding canvas screen

--- gui.py
+++ gui.py
@@ -19,11 +19,26 @@ class gui_system():
     def layout(self):
         main=tk.Frame(self.root)
         main.pack(fill=tk.BOTH, expand=True)
-        panel=tk.Frame(main)
-        panel.pack(side=tk.BOTTOM)
+        panel=tk.Frame(main, width=200)
+        panel.pack(side=tk.LEFT, fill=tk.Y)
 
-        tk.Button(panel, text="Greyscale").pack(side=tk.LEFT, padx=15, pady=15)
-        tk.Button(panel, text="Blur").pack(side=tk.LEFT, padx=15, pady=15)
-        tk.Button(panel, text="Edges").pack(side=tk.LEFT, padx=15, pady=15)
-        tk.Button(panel, text="Rotate").pack(side=tk.LEFT, padx=15, pady=15)
-        tk.Button(panel, text="Flip").pack(side=tk.LEFT, padx=15, pady=15)
+        tk.Button(panel, text="Greyscale").pack(fill=tk.X, padx=15, pady=5)
+        tk.Button(panel, text="Blur").pack(fill=tk.X, padx=15, pady=5)
+        tk.Button(panel, text="Edges").pack(fill=tk.X, padx=15, pady=5)
+        tk.Button(panel, text="Rotate 90°").pack(fill=tk.X, padx=10, pady=5)
+        tk.Button(panel, text="Flip Horizontal").pack(fill=tk.X, padx=10, pady=5)
+
+        canvas_frame=tk.Frame(main)
+        canvas_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.canvas=tk.Canvas(canvas_frame, bg="black")
+        self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def status(self):
+        #to do
+    def display(self):
+        #to do
+    def saveas(self):
+        #to do
+
+
