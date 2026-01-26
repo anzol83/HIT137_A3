84fcde75d5f4e08bdef6e3c8c9b6d3c75263e260
Avaya Manandhar <shresthaavaya999@gmail.com>
2026-01-26 16:35:21 +0930

    Adding functions

--- functions.py
+++ functions.py
@@ -9,53 +9,60 @@ class Functions:
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.processor.grayscale()
 
         
     def blur(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.processor.blur()
 
 
     def edges(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.processor.edges()
 
 
     def rotate_90(self):
        if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.processor.rotate(angle=90)
        
         # to do
     def flip_horizontal(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.processor.flip(horizontal=True)
 
 
     def brightness(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
-        
+        self.processor.brightness_value = int(value)
+
         
     def scale(self):
         if self.processor.image is None:
             return
-        self.history.save(self.processor.image)
+        self.processor.scale_value = int(value) / 100
 
 
     def undo(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
+        self.processor.image = self.history.undo(self.processor.image)
 
-        
+
     def redo(self):
         if self.processor.image is None:
             return
         self.history.save(self.processor.image)
-
-    
+        self.processor.image = self.history.redo(self.processor.image)
+        
