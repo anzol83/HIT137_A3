f98bf501ba9be9881352fd3365e4ade4f40cff81
Avaya Manandhar <shresthaavaya999@gmail.com>
2026-01-26 15:46:38 +0930

    adding variables

--- functions.py
+++ functions.py
@@ -1,8 +1,27 @@
 class Functions:
-    def __init__(self):
 
-    def grayscale(self):
+    def __init__(self, processor, history, display_callback):
+        self.processor = processor
+        self.history = history
+        self.display_callback = display_callback
         
+    def grayscale(self):
+        # to do
     def blur(self):
+
+    def edges(self):
+
+    def rotate_90(self):
+        # to do
+    def flip_horizontal(self):
+
+    def brightness(self):
+        # to do
+    def scale(self):
+
+    def undo(self):
+        # to do
+    def redo(self):
+    
 
     
