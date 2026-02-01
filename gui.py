3f5dd4db68f62bfb5cdb7c5ad895dc394c9c9b45
LEGION <rahuladhk0@gmail.com>
2026-02-01 12:48:41 +0930

    adding modules and functions

--- gui.py
+++ gui.py
@@ -1,10 +1,16 @@
 import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+
+from functions import Functions
 
 class gui_system():
     def __init__(self, root):
         self.root=root
         self.root.title("Image Editor")
         self.root.geometry("600x400")
+
+        self.func=Functions(self.display)
         
         self.window()
         self.layout()
@@ -13,11 +19,12 @@ class gui_system():
         menu=tk.Menu(self.root)
         
         file_menu=tk.Menu(menu, tearoff=0)
-        file_menu.add_cascade(label="open")
-        file_menu.add_cascade(label="Save As")
-        file_menu.add_cascade(label="Exit")
-        
+        file_menu.add_command(label="Open")
+        file_menu.add_command(label="Save As")
+        file_menu.add_command(label="Exit")
+
         menu.add_cascade(label="File")
+        menu.add_cascade(label="Edit")
 
         self.root.config(menu=menu)
 
@@ -39,3 +46,22 @@ class gui_system():
         self.canvas=tk.Canvas(canvas_frame, bg="black")
         self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+    def create_status_bar(self):
+        self.status = tk.Label(self.root, text="No image loaded",
+                               bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.status.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def display(self, img):
+
+        if len(img.shape) == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        else:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        im = Image.fromarray(img)
+        self.tk_img = ImageTk.PhotoImage(im)
+
+        self.canvas.delete("all")
+        self.canvas.create_image(10, 10, anchor=tk.NW, image=self.tk_img)
+        self.last_displayed_image=img
+        return img
