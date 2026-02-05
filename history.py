9d7834a6131f1055eea53a694032d6a0e43f2fac
iDevilGamer <erankhattri@gmail.com>
2026-02-05 12:15:15 +0930

    Adding redo function

--- history.py
+++ history.py
@@ -1,5 +1,5 @@
 """
-Adding saving option 
+Adding saving, undo option 
 """
 def __init__(self):
     self.undo_stack = []
@@ -16,4 +16,10 @@ def undo(self, current):
     if self.undo_stack and current is not None:
         self.redo_stack.append(current)
         return self.undo_stack.pop()
+    return current
+
+def redo(self, current):
+    if self.redo_stack and current is not None:
+        self.undo_stack.append(current)
+        return self.redo_stack.pop()
     return current
