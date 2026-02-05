7b0a7e9e6f9527a84f2f5510d6b32495e74b0d39
iDevilGamer <erankhattri@gmail.com>
2026-02-05 12:18:41 +0930

    Adding History class and fixing indentation

--- history.py
+++ history.py
@@ -1,25 +1,26 @@
 """
-Adding saving, undo option 
+Adding saving, undo & redo option for editing
 """
-def __init__(self):
-    self.undo_stack = []
-    self.redo_stack = []
+class History:
+    def __init__(self):
+        self.undo_stack = []
+        self.redo_stack = []
 
 
-def save(self, image):
-    if image is None:
-        return
-    self.undo_stack.append(image.copy())
-    self.redo_stack.clear()
+    def save(self, image):
+        if image is None:
+            return
+        self.undo_stack.append(image.copy())
+        self.redo_stack.clear()
 
-def undo(self, current):
-    if self.undo_stack and current is not None:
-        self.redo_stack.append(current)
-        return self.undo_stack.pop()
-    return current
+    def undo(self, current):
+        if self.undo_stack and current is not None:
+            self.redo_stack.append(current)
+            return self.undo_stack.pop()
+        return current
 
-def redo(self, current):
-    if self.redo_stack and current is not None:
-        self.undo_stack.append(current)
-        return self.redo_stack.pop()
-    return current
+    def redo(self, current):
+        if self.redo_stack and current is not None:
+            self.undo_stack.append(current)
+            return self.redo_stack.pop()
+        return current
