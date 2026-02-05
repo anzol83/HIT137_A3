947e9d2abbea4ffce2ae3758dbc5e97372d9b48e
Angel Neupane <angelneupane83@gmail.com>
2026-02-05 13:17:11 +0930

    2

--- imageProcessor.py
+++ imageProcessor.py
@@ -1,2 +1,11 @@
+import cv2
+
 class ImageProcessor:
     """A simple OpenCV-based image processor for basic transformations."""
+
+def __init__(self):
+        self.image = None
+        self.original = None
+        self.filename = None
+        self.brightness_value = 0
+        self.scale_value = 1.0
